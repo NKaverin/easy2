c0851c1097696b5caf28006b50fecc4b708617d0
Nikolay Kaverin <n.kaverin.91@gmail.com>
2021-11-01 20:15:11 +0300

    asd

---
+++ script.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper('.gallery__swiper', {
+    navigation: {
+        nextEl: '.gallery__swiper-right',
+        prevEl: '.gallery__swiper-left',
+    },
+    spaceBetween: 40,
+});
+
+const swiper2 = new Swiper('.press__swiper', {
+    pagination: {
+        el: '.press__pagination',
+        clickable: true,
+        horizontalClass: '.press__pagination'
+    },
+
+    
+
+});
+
+
+
+let menuLinks= document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', function(e) {
+            let menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                let gotoBlock = document.querySelector(menuLink.dataset.goto);
+                let gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: 'smooth'
+                });
+                e.preventDefault();
+            }
+        })
+    });
+}
+
